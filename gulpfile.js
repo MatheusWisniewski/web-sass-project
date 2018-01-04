1cbe080ed1f212c0bc33e62157c96fe33650c16c
Matheus Wisniewski <matheus.maw@gmail.com>
2018-01-04 18:32:48 -0200

    Added browser-sync and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
 
-
-
+var SOURCE_PATHS = {
+    sassSource: 'src/scss/*.scss'
+}
+var APP_PATHS = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(SOURCE_PATHS.sassSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest(APP_PATHS.css))
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function() {
+    browserSync.init(
+        [
+            APP_PATHS.root + '/*.html', 
+            APP_PATHS.css + '/*.css',
+            APP_PATHS.js + '/*.js'
+        ], 
+        {
+            server: {
+                baseDir: APP_PATHS.root
+            }
+        }
+    )
+})
+
+gulp.task('default', ['serve']);
